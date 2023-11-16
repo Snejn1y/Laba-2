c17107dfa9a25efd5845b6017e8ac72114668a78
Snejn1y <cheburatovich@gmail.com>
2023-11-16 13:29:23 +0200

    Add project files.

---
+++ Laba 2/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Laba_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            var product = new Product(
+                Convert.ToInt32(quantity.Text),
+                Convert.ToInt32(price.Text));
+
+            var mark = new Markers(
+                Convert.ToString(name_Copy.Text),
+                Convert.ToInt32(quantity_Copy.Text),
+                Convert.ToInt32(price_Copy.Text),
+                Convert.ToInt32(sort.Text));
+
+            total.Content = product.GetTotalPrice().ToString();
+            total_Copy.Content = mark.GetTotalPrice().ToString();
+        }
+    }
+}
---
+++ Laba 2/Product.cs
@@ -0,0 +1,43 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba_2
+{
+    public class Product
+    {
+        protected int quantity;
+        protected double price;
+
+        public Product( int quantity, double price)
+        {
+            this.quantity = quantity;
+            this.price = price;
+        }
+
+        public double GetTotalPrice()
+        {
+            return quantity * price;
+        }
+    }
+
+    public class Markers : Product
+    {
+        protected string name;
+        protected int grade;
+
+        public Markers(string name, int quantity, double price, int grade) : base(quantity, price)
+        {
+            this.name = name;
+            this.grade = grade;
+        }
+
+        public new double GetTotalPrice()
+        {
+            return base.GetTotalPrice() / grade;
+        }
+    }
+    
+}
